b7128d31324777883ca003a0d9eec9398214175a
Arnold45202 <143671110+Arnold45202@users.noreply.github.com>
2024-01-17 22:15:10 +1300

    adding images into the website

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import '../styleSheets/Home.css'
+/* Home.js */
+
+import React from 'react';
+import '../styleSheets/Home.css';
 
 function Home() {
   return (
     <div className='home'>
+      <div className='logo-container'>
+        <img src='/images/UnswLogo.png' className='logo' />
+      </div>
+      <div className='text-container'>
         <h1 className='title'>
-            Passionate Software Engineering<br></br> student @UNSW
+          Passionate Software Engineering student @UNSW
         </h1>
-        <p className='description'>Hey, I'm Ka Ho Wang other wise known as Jackson, <br />A student looking for opportuniteis</p>
-        <img src="thomas.jpg"/>
-
+        <p className='description'>
+          Hey! I'm Jackson Wang, always eager to learn, <br />A student looking for opportunities based in <br /> Sydney, Australia 📍
+        </p>
+      </div>
+      <img src='/images/thumbsup.jpg' alt='Thumbs Up' className='img' />
     </div>
   );
 }
