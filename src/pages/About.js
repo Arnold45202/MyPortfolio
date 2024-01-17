245da7ace3cc3505c4885d0a6ed045ae20ca79a4
Arnold45202 <143671110+Arnold45202@users.noreply.github.com>
2024-01-17 22:58:58 +1300

    i added like the about me page and need to fix up the css after

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React from 'react';
+import '../styleSheets/About.css';
 
 function About() {
   return (
-    <div>About Me</div>
+    <div>
+      <h1>About me</h1>
+      <section>
+        <h1>Hey there, I'm Jackson Wang!</h1>
+        <p>Welcome to my corner of the digital world.</p>
+      </section>
+
+      <section>
+        <h2>How did I get into coding?</h2>
+        <p>
+        So, it might sound a bit wild, but coding wasn't my first go-to. Initially, I thought about diving into the whole med scene because, 
+        you know, all my friends were into it. 
+        But one day, reality hit, and I realised I didn't actually want to be a doctor. <br />
+        Turns out, my heart was in engineering. I've always been that tech kid. Back in the day, during year 9 to 12, I was building computers and fixing phones on the side for money. 
+        Tech videos were my jam, and then AI dropped, taking my excitement to a whole new level. <br />
+        It wasn't until year 13 that I figured coding was my thing. Took my first computer science course, and bam, fell in love with it. 
+        Now, I'm hooked on learning new stuff like React, HTML, CSS, and Python. My school wasn't able to teach me these stuff for my first yaer, but hey, 
+        I couldn't wait for the holidays to get my code on, so I started doing it myself!!
+        </p>
+      </section>
+
+      <section>
+        <h2>My Hobbies!</h2>
+        <p>
+          Coding and making projects 💻<br />
+          Hanging out with family and friends 👨‍👩‍👧‍👦<br />
+          Playing single-player games like rdr2 and a large gta fan can't wait for gta 6 ! 🎮 
+        </p>
+      </section>
+
+      <section>
+        <h2>What are some fun facts about me</h2>
+        <p>
+          My favourite anime is attack on titan 🔰<br />
+          I was the deputy Head prefect for my College had to give speeches 😳<br />
+          Hard-stuck platinum in league of legends ⚔️<br />
+          My WPM for 15 seconds is like 150 and for one minute it's like around 130 ⌨️<br />
+        </p>
+      </section>
+
+
+
+    </div>
   );
 }
 
