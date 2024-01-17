38b9dce000d434309863eebd4d93633800978809
Arnold45202 <143671110+Arnold45202@users.noreply.github.com>
2024-01-17 19:08:49 +1300

    aadding the navbar and the footer and made multiple routes

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import '../styleSheets/Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div>
+        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+          <InstagramIcon />
+        </a>
+        <a href="https://www.linkedin.com/in/jackson-wang-75357129a/" target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon />
+        </a>
+        <a href="https://github.com/Arnold45202" target="_blank" rel="noopener noreferrer">
+          <GitHubIcon />
+        </a>
+        <p>&copy; 2024 JacksonWang.github.io</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styleSheets/Navbar.css';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='buttons'>
+            {/* <button></button> */}
+        </div>
+        <div className='links'>
+            <Link to="/" className="nav-link"> Home </Link>
+            <Link to="/about" className="nav-link"> About Me </Link>
+            <Link to="/projects" className="nav-link"> Projects </Link>
+            <Link to="/contacts" className="nav-link"> Contacts </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
