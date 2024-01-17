61a35282ac464f7a09700ae56319a3449617f04c
Arnold45202 <143671110+Arnold45202@users.noreply.github.com>
2024-01-17 20:15:22 +1300

    adding a header and description along side with a picture

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../styleSheets/Home.css'
 
 function Home() {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+        <h1 className='title'>
+            Passionate Software Engineering<br></br> student @UNSW
+        </h1>
+        <p className='description'>Hey, I'm Ka Ho Wang other wise known as Jackson, <br />A student looking for opportuniteis</p>
+        <img src="thomas.jpg"/>
+
+    </div>
   );
 }
 
