29e5e38dc1532055dc7d76c735f107d490238656
Arnold45202 <143671110+Arnold45202@users.noreply.github.com>
2024-01-18 13:23:45 +1300

    adding one of my projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,25 @@
-import React from 'react'
+// Projects.js
+import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import '../styleSheets/Projects.css';
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>
+      <div className="project-container">
+        <h1 className='title3'>My Projects!</h1>
+        <div className="project-box">
+          <img className='gamePicture' src="https://cdn.discordapp.com/attachments/949224467127746610/1197335073003941949/GameRush_title_logo.png?ex=65bae42e&is=65a86f2e&hm=92d486c73b8be3cccc9ea20c05043665b71953d74f6802c4f1bb7611f0661eb7&" alt="Project Image" />
+          <div className="project-details">
+            <h2 className="project-title"><a href="https://arcadegamedown.000webhostapp.com/index.html" target="_blank" rel="noopener noreferrer" className='Project-title'>Game Rush</a></h2>
+            <p>This is a game website where I got to learn how to make a game website, where it has like the childhood games such as like tic tac toe etc, By doing this I learnt how to code using html/css and python.</p>
+            <a href="https://github.com/Arnold45202/Games" target="_blank" rel="noopener noreferrer">
+              <GitHubIcon />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
