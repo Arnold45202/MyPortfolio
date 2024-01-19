04f42305e47f42b1891b831ffcbb7eda85555c96
Arnold45202 <143671110+Arnold45202@users.noreply.github.com>
2024-01-19 22:04:42 +1300

    adding a helmat to my website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -11,6 +12,11 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <Router>
+      <Helmet>
+        <meta property="og:title" content="Jackson's Portfolio" />
+        <meta property="og:description" content="Hi, I built this website to farmlise myself with react and to showcase my skill/projects and a little about me :)" />
+      </Helmet>
+
       <Navbar />
       <Routes>
         <Route path="/MyPortfolio" element={<Home />} />
