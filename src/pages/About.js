010276d7c6cc55b8c1427d2200ce0b6ba7e9669a
Arnold45202 <143671110+Arnold45202@users.noreply.github.com>
2024-01-19 13:00:53 +1300

    fixing my home picture

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,12 @@ import '../styleSheets/About.css';
 
 function About() {
   return (
-    <div>
+    <div className='about-container'>
       <div className='writing'>
         <section>
           <h1 className='toppp'>About Me</h1>
-          <p className='first-part'>19 year old From New Zealand, studying Software Engineering at UNSW</p>
+          <p className='first-part'>19 year old From New Zealand, studying Software Engineering at UNSW <br></br> 
+          Currently self learning Object-oriented programming and cloud computing by aws.</p>
         </section>
 
         <section>
