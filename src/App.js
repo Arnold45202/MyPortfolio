3c04fd6fc8742aacc67d1b6b98070c54af7f6739
Jackson <kahowang3659@gmail.com>
2024-02-11 19:33:00 +1100

    changing public to docs

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
         <meta property="og:title" content="Jackson's Portfolio" />
         <meta property="og:description" content="Hi, I built this website to familiarise myself with react and to showcase my skills/projects and a little about me :)" />
       </Helmet>
-
+      
       <Navbar />
       <Routes>
         <Route path="/MyPortfolio" element={<Home />} />
