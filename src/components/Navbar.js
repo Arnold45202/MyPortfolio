a57c100e6380fe591225339d9b35648f5e9452ca
Jackson <kahowang3659@gmail.com>
2024-02-11 19:20:46 +1100

    fixing the projects side and changing contacts to resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
             <Link to="/MyPortfolio" className="nav-link"> Home </Link>
             <Link to="/about" className="nav-link"> About Me </Link>
             <Link to="/projects" className="nav-link"> Projects </Link>
-            <a href='https://www.youtube.com/' className="nav-link" target="_blank" rel="noopener noreferrer"> Resume </a>
+            <a href='https://drive.google.com/file/d/1jbZEj_w5e9sRa_FmcJ4vIXJWZuDcylv-/view?usp=sharing' className="nav-link" target="_blank" rel="noopener noreferrer"> Resume </a>
         </div>
     </div>
   )
